293782759e650ef0d316ff634cc194f62ddb0d04
Samuel Leavin <sam.leavin@att.net>
2015-06-28 14:30:26 -0400

    updated home.html with rows

--- extension/js/background.js
+++ extension/js/background.js
@@ -3,3 +3,16 @@ function logInBackground (dataToLog) {
 	console.log('In background.js');
 	console.log(dataToLog);
 }
+
+function tabGetter () {
+    chrome.tabs.getSelected(null, function(tab) {
+      console.log('the tab argument: ', tab);
+    });
+}
+
+function reqBodyIntercept() {
+	console.log('listening...')
+  chrome.webRequest.onBeforeRequest.addListener(function (data) {
+      console.log('the data: ', data);
+  }, {urls: ["<all_urls>"]}, ["blocking", "requestBody"])
+}
