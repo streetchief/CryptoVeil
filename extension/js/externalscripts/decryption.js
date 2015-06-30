fcbd10876a6a4037776da5c904562cf5435a9917
Thomas Proenza <tomproenza@Thomass-MacBook-Pro.local>
2015-06-30 12:51:56 -0400

    added unhacking function and successfully can encrypt and decrypt email messages

--- extension/js/externalscripts/decryption.js
+++ extension/js/externalscripts/decryption.js
@@ -6,6 +6,7 @@ var decryptedMain = function () {
 	var regEx = /%%%%(.+)%%%%/gmi
   var sentinel = '%%%%';
   var sentinelLength = sentinel.length;
+  var unhacked;
 
 	gmail2.observe.on("view_thread", function (thread) {});
 
@@ -14,17 +15,29 @@ var decryptedMain = function () {
 		var email = new gmail2.dom.email(email.id)
 		var body = email.body();
 
+		//ELIMINATING POSSIBLE TAGS INSERTED BY CHROME
+		tagsRegEx = /<{1}.+?>{1}/g
+		body = body.replace(tagsRegEx, "");
+
+
 		if(body.indexOf(sentinel) > -1) {
 			var encryptedMsg = body.match(regEx)[0].slice(sentinelLength, -sentinelLength)
-			console.log('body', encryptedMsg);
+			console.log('the encryptedMsg: ', encryptedMsg);
 			// sendToContentScript(body);
+			unhacked = unhacking(encryptedMsg);
 		} 
 		//THIS IS FOR VISUAL FEEDBACK THAT IT'S WORKING
-		email.body('<h1>%CryptoVeil%</h1>' + body)
+		email.body(unhacked + '<h5>Decrypted by CryptoVeil</h5>')
 	})
 };
 
-/*
-tagsRegEx = /<{1}.+?>{1}/g,
-regEx = new RegExp(keyword, 'gi');
-*/
+		function unhacking(text) {
+		    console.log('hit unhacking', text)
+		    // var encryptedText = text.slice(15,-6);
+		    var decrypted = CryptoJS.AES.decrypt(text, "Secret Passphrase");
+		    var temp = decrypted.toString(CryptoJS.enc.Utf8);
+		    console.log('this is the decrypted message', temp)
+		    return temp;
+		}
+
+
