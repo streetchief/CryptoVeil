5d1815a552b3fb0156e9d31e804728dfd5eaaa01
Rachel Lee <lee.rachel.jy@gmail.com>
2015-06-30 11:44:50 -0400

    start working on register

---
+++ extension/js/application/states/register/register.js
@@ -0,0 +1,31 @@
+app.config(function ($stateProvider) {
+
+    $stateProvider.state('register', {
+        url: '/register',
+        templateUrl: 'js/application/states/register/register.html',
+        controller: 'registerController'
+    });
+
+});
+
+app.controller('registerController', function ($scope, AuthService, $state) {
+
+    $scope.register = {};
+    $scope.error = null;
+
+    $scope.createUser = function (user) {
+
+        $scope.error = null;
+
+        UserFactory.createUser(user)
+        .then(function() {
+            return AuthService.login(user);
+        })
+        .then(function () {
+            $state.go('home');
+        }).catch(function () {
+            $scope.error = 'Invalid credentials.';
+        });
+    };
+
+});
