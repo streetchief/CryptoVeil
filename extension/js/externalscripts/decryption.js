ccec748eaf9f62c10ed5e35562956d8815997345
Samuel Leavin <sam.leavin@att.net>
2015-07-02 20:53:04 -0400

    more small cleanup

--- extension/js/externalscripts/decryption.js
+++ extension/js/externalscripts/decryption.js
@@ -1,19 +1,22 @@
 //////////////////////////	DECRYPTION	///////////////////////////////////
 var decryptedMain = function () {
 	gmail2 = new Gmail();
+
 	console.log('Hello from decryptedMain,', gmail2.get.user_email());
 
-	var regEx = /%%%%(.+)%%%%/gmi
-  var sentinel = '%%%%';
-  var sentinelLength = sentinel.length;
-  var unhacked;
+	var regEx = /%%%%(.+)%%%%/gmi,
+		sentinel = '%%%%',
+		sentinelLength = sentinel.length,
+		unhacked;
 
 	gmail2.observe.on("view_thread", function (thread) {});
 
 	gmail2.observe.on("view_email", function (email) {
+
+		var email, body, encryptedMsg;
 	
-		var email = new gmail2.dom.email(email.id)
-		var body = email.body();
+		email = new gmail2.dom.email(email.id)
+		body = email.body();
 
 		//ELIMINATING POSSIBLE TAGS INSERTED BY CHROME
 		tagsRegEx = /<{1}.+?>{1}/g
@@ -21,22 +24,22 @@ var decryptedMain = function () {
 
 
 		if(body.indexOf(sentinel) > -1) {
-			var encryptedMsg = body.match(regEx)[0].slice(sentinelLength, -sentinelLength)
-			console.log('the encryptedMsg: ', encryptedMsg);
+
+			encryptedMsg = body.match(regEx)[0].slice(sentinelLength, -sentinelLength)
+
 			// sendToContentScript(body);
 			unhacked = unhacking(encryptedMsg);
 			email.body(unhacked + '<h5>Decrypted by CryptoVeil</h5>')
 		} 
-	})
+	});
 };
 
 function unhacking(text) {
-    console.log('hit unhacking', text);
-    
-    var decrypted = CryptoJS.AES.decrypt(text, "Secret Passphrase");
-    var temp = decrypted.toString(CryptoJS.enc.Utf8);
 
-    console.log('this is the decrypted message', temp);
+	var decrypted, temp;
+    
+    decrypted = CryptoJS.AES.decrypt(text, "Secret Passphrase");
+    temp = decrypted.toString(CryptoJS.enc.Utf8);
 
     return temp;
 }
