517e76bdf76da5833c4e24bbb6780049c4fdb449
Thomas Proenza <gojehs7@gmail.com>
2015-07-06 18:00:27 -0400

    decryption with assigned circle works

--- extension/js/externalscripts/decryption.js
+++ extension/js/externalscripts/decryption.js
@@ -7,14 +7,22 @@ var decryptedMain = function () {
 	var regEx = /%%%%(.+)%%%%/gmi,
 		sentinel = '%%%%',
 		sentinelLength = sentinel.length,
-		key = 'Secret Passphrase',
+		selectedCircleKey,
+		selectedCircleId,
+		selectedCircleName,
 		unhacked;
+
+	document.addEventListener('set-encryption-circle', function(e) {
+		selectedCircleKey = e.detail.key;
+		selectedCircleId = e.detail._id;
+		selectedCircleName = e.detail.name;
+	});
 
 	gmail2.observe.on("view_thread", function (thread) {});
 
 	gmail2.observe.on("view_email", function (email) {
 
-		var email, body, encryptedMsg;
+		var email, body, encryptedMsg, circleId;
 	
 		email = new gmail2.dom.email(email.id)
 		body = email.body();
@@ -26,11 +34,13 @@ var decryptedMain = function () {
 
 		if(body.indexOf(sentinel) > -1) {
 
-			encryptedMsg = body.match(regEx)[0].slice(sentinelLength, -sentinelLength)
+			encryptedMsg = body.match(regEx)[0].slice(sentinelLength, -sentinelLength);
+			circleId = encryptedMsg.slice(-24);
+			encryptedMsg = encryptedMsg.slice(0, -24);
 
 			// sendToContentScript(body);
-			unhacked = decrypt(encryptedMsg, key);
-			email.body(unhacked + '<h5>Decrypted by CryptoVeil</h5>')
+			unhacked = decrypt(encryptedMsg, selectedCircleKey);
+			email.body(unhacked + '<h5>' + selectedCircleName +' | Decrypted by CryptoVeil</h5>')
 		} 
 	});
 };
