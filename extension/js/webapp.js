718d796f18428670f247ab174c3bd1cc1e7637ab
Rachel Lee <lee.rachel.jy@gmail.com>
2015-07-07 16:30:18 -0400

    commented out refresh page on the listener in webapp.js

--- extension/js/webapp.js
+++ extension/js/webapp.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
 	var server = 'http://127.0.0.1:1337'
 
 	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	    window.location.href = server;
+	    console.log('this is listening to the backgroundFactory')
+	    // window.location.href = server;
 	});
 
 });
