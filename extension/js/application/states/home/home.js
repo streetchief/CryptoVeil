f5e3d936f459bc001fc274af3fdfd2117b77e4e4
Rachel Lee <lee.rachel.jy@gmail.com>
2015-07-11 23:37:48 -0400

    add more design

--- extension/js/application/states/home/home.js
+++ extension/js/application/states/home/home.js
@@ -22,9 +22,8 @@ app.controller('homeController', function ($scope, BackgroundFactory, $log, togg
 
   backgroundPage = BackgroundFactory.getBackgroundPage();
   $scope.encryptionState = toggleState;
-  encryptionOffMessage = 'Encryption OFF';
-  encryptionOnMessage = 'Encryption ON';
-
+  encryptionOffMessage = 'Encryption is now OFF';
+  encryptionOnMessage = 'Encryption is now ON';
 
   if (toggleState) {
     $scope.stateMsg = encryptionOnMessage;
