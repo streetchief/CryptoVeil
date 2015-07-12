3254170ab35ade8064cb376ab9b7976a9ec714c6
Samuel Leavin <sam.leavin@gmail.com>
2015-07-12 13:59:03 -0400

    switching background from promise to q

--- extension/js/application/factories/CircleFactory.js
+++ extension/js/application/factories/CircleFactory.js
@@ -1,8 +1,9 @@
-app.factory('CircleFactory', function ($http) {
+app.factory('CircleFactory', function ($http, $log) {
 
     var backgroundPage = chrome.extension.getBackgroundPage();
     var currentLoggedUser = backgroundPage.user.getLoggedInUser();
     var server = 'http://127.0.0.1:1337';
+    var clog = $log.info;
     
     var composeRequest = function (method, url, data) {
         return {
@@ -38,8 +39,13 @@ app.factory('CircleFactory', function ($http) {
 
         editMember: function(circleId, memberEmail, edit){
             // edit is add or delete
-            return $http(composeRequest('PUT', '/api/circles/' + circleId, {newEmail: memberEmail, edit: edit}))
-            .then(function(response){
+            clog('circleId', circleId, 'email', memberEmail, 'command', edit);
+
+            var options = {newEmail: memberEmail, edit: edit};
+
+            return $http(composeRequest('PUT', '/api/circles/' + circleId, options))
+            .then(function (response) {
+                clog('inside circlefactory.editMember response',response)
                 return response.data;
             });
         }
